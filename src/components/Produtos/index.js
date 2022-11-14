f452151eac9327242e468cb60f14f7ab2122ce87
juliomabilio <juliofurtadomabilio@gmail.com>
2022-11-14 15:03:10 +0000

    alteração na section navbar

--- src/components/Produtos/index.js
+++ src/components/Produtos/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './style.css'
+import { StaticImage } from "gatsby-plugin-image"
 import prod1 from '../../images/pc1.jpg'
 import prod2 from '../../images/pc2.jpg'
 import prod3 from '../../images/pc3.jpg'
@@ -8,10 +9,20 @@ import {AiFillStar,AiOutlineStar,AiFillShopping} from 'react-icons/ai'
 
 const Produtos = () => {
   return (
-    <div className='produtos padding'>
+    <div className='produtos padding' id='tecnlogia'>
       <div className='container'>
+        <h3>TECNOLOGIA</h3>
+        <div className='row'>
         <div className='card'>
           <div className='imagem'>
+          <StaticImage
+      src="../images/dino.png"
+      alt="A dinosaur"
+      placeholder="blurred"
+      layout="fixed"
+      width={200}
+      height={200}
+    />
             <img src={prod1} alt='computador'/>
           </div>
           <div className='info'>
@@ -148,7 +159,7 @@ const Produtos = () => {
             <a href='#'  className='Ai'><AiFillShopping/></a> 
           </div>
         </div>
-        <div className='card'>
+        {/* <div className='card'>
           <div className='imagem'>
             <img src={prod2} alt='computador'/>
           </div>
@@ -170,8 +181,8 @@ const Produtos = () => {
             <a href='#'><AiFillShopping/></a> 
             <a href='#'><AiFillShopping/></a> 
           </div>
-        </div>
-        <div className='card'>
+        </div> */}
+        {/* <div className='card'>
           <div className='imagem'>
             <img src={prod3} alt='computador'/>
           </div>
@@ -193,8 +204,8 @@ const Produtos = () => {
             <a href='#'><AiFillShopping/></a> 
             <a href='#'><AiFillShopping/></a> 
           </div>
-        </div>
-        <div className='card'>
+        </div> */}
+        {/* <div className='card'>
           <div className='imagem'>
             <img src={prod1} alt='computador'/>
           </div>
@@ -216,6 +227,7 @@ const Produtos = () => {
             <a href='#'><AiFillShopping/></a> 
             <a href='#'><AiFillShopping/></a> 
           </div>
+        </div> */}
         </div>
       </div>
     </div>
